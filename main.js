7625f8b27125ca0c3b30d84a0d8ecc22796eb066
hknktk <75674444+hknktk@users.noreply.github.com>
2021-02-24 16:02:05 +0900

    commit
    更新した。

--- main.js
+++ main.js
@@ -1,34 +1,56 @@
 document.addEventListener('DOMContentLoaded',function(){
+  const $menuButton = document.getElementById("menuButton");
+  const $slide      = document.getElementById("slide");
+  const $mask       = document.getElementById("mask");
+  const $list     = document.getElementById("slideList");
+
+  
   document.getElementById("menuButton").addEventListener("click",function(){
-    this.classList.toggle("active");
-    document.getElementById("nav").classList.toggle("active");
-    document.getElementById("mask").classList.toggle("active");
-    document.getElementById("bar").classList.toggle("active");
-    document.getElementById("topMenu").classList.toggle("active");
-    document.getElementById("miniMenu").classList.toggle("active");
-    document.querySelector("html").classList.toggle("none");
-    document.querySelector("body").classList.toggle("none");
-
-  });
-
-
+    $menuButton.classList.toggle("active");
+    $slide.classList.toggle("active");
+    $mask.classList.toggle("active"); 
+  })
+  
   document.getElementById("mask").addEventListener("click",function(){
-    this.classList.toggle("active");
-    document.getElementById("nav").classList.toggle("active");
-    document.querySelector("html").classList.toggle("none");
-    document.querySelector("body").classList.toggle("none");
-    document.getElementById("menuButton").classList.toggle("active");
-    document.getElementById("bar").classList.toggle("active");
+    $menuButton.classList.toggle("active");
+    $mask.classList.toggle("active");
+    $slide.classList.toggle("active");
+  
   })
 
-  document.getElementById("list").addEventListener("click",function(){
-    document.getElementById("nav").classList.toggle("active");
-    document.querySelector("html").classList.toggle("none");
-    document.querySelector("body").classList.toggle("none");
-    document.getElementById("menuButton").classList.toggle("active");
-    document.getElementById("mask").classList.toggle("active");
-    document.getElementById("bar").classList.toggle("active");
+  document.getElementById("slideList").addEventListener("click",function(){
+    $menuButton.classList.toggle("active");
+    $mask.classList.toggle("active");
+    $slide.classList.toggle("active");
+    
   })
+
+
+    const targetElement = document.querySelectorAll(".js-text");
+    document.addEventListener("scroll", function(){
+    for (let i = 0; i<targetElement.length; i++){
+    const getElementDistance =targetElement[i].
+    getBoundingClientRect().top + targetElement[i].clientHeight * .9
+       if(window.innerHeight > getElementDistance){
+       targetElement[i].classList.add("show");
+        }
+      }
+    })
+
+
+    const targetElement2 = document.querySelectorAll(".js-sub");
+    document.addEventListener("scroll", function(){
+    for (let i = 0; i<targetElement2.length; i++){
+    const getElementDistance =targetElement2[i].
+    getBoundingClientRect().top + targetElement2[i].clientHeight * .9
+       if(window.innerHeight > getElementDistance){
+       targetElement2[i].classList.add("show");
+        }
+      }
+    })
+
+
+
 
 })
 
